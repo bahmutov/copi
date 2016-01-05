19c531744b5059651502036d12d4123da2872845
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-01-04 23:35:48 -0500

    feat(cache): finding package filenames and caching them

---
+++ src/cache.js
@@ -0,0 +1,44 @@
+const is = require('check-more-types')
+const la = require('lazy-ass')
+const fs = require('fs')
+
+function saveData (filename, data) {
+  la(is.unemptyString(filename), 'missing filename', filename)
+
+  const withTimestamp = {
+    data: data,
+    timestamp: new Date()
+  }
+  fs.writeFileSync(filename, JSON.stringify(withTimestamp, null, 2), 'utf-8')
+  console.log('saved data to %s', filename)
+  return data
+}
+
+function loadData (maxAge, filename) {
+  la(is.unemptyString(filename), 'missing filename', filename)
+  if (!fs.existsSync(filename)) {
+    return null
+  }
+  const text = fs.readFileSync(filename, 'utf-8')
+  const withTimestamp = JSON.parse(text)
+  la(is.has(withTimestamp, 'timestamp'), 'missing timestamp', Object.keys(withTimestamp))
+
+  const now = new Date()
+  const elapsed = now - new Date(withTimestamp.timestamp)
+  console.log('elapsed', elapsed, 'ms, maxAge', maxAge)
+  if (elapsed > maxAge) {
+    return null
+  }
+
+  la(is.has(withTimestamp, 'data'), 'missing data', Object.keys(withTimestamp))
+  return withTimestamp.data
+}
+
+function makeCache (filename, maxAge) {
+  return {
+    save: saveData.bind(null, filename),
+    load: loadData.bind(null, maxAge, filename)
+  }
+}
+
+module.exports = makeCache
--- src/copi.js
+++ src/copi.js
@@ -1,5 +1,37 @@
+const glob = require('glob-promise')
+const is = require('check-more-types')
+const la = require('lazy-ass')
+const makeCache = require('./cache')
+
+function findPackageFiles (folder) {
+  return glob(folder + '/*/node_modules/*/package.json')
+}
+
+function seconds (n) {
+  return n * 1000
+}
+
+const filenamesCache = makeCache(__dirname + '/../.found-packages.json', seconds(300))
+
 function copi (options) {
   console.log('installing %s', options.name)
+
+  const rootFolder = options.folder || process.cwd() + '/../'
+  return Promise.resolve(filenamesCache.load())
+    .then(function (filenames) {
+      if (!filenames) {
+        return findPackageFiles(rootFolder)
+          .then(function (filenames) {
+            la(is.array(filenames), 'could not package filenames', filenames)
+            console.log('found %d package filenames', filenames.length)
+            return filenames
+          })
+      }
+      console.log('using cached %d package filenames', filenames.length)
+      return filenames
+    })
+    .then(filenamesCache.save)
+    .catch(console.error.bind(console))
 }
 
 module.exports = copi
